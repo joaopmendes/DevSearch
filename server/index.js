4c23b8460b4dfdd27e29c3160e024de49185a326
joaopmendes <joaopmendes@gmail.com>
2020-01-14 17:49:40 +0000

    Changed index.js to server folder

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express()
 const port = 9000
 app.listen(port)
+app.use(express.json)
 console.log("[i] Listening on port:", port)
 
 app.get("/", function(req, res) {
