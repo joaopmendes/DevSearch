9c1bad8fb2e0fd0dcc9e2991024a103279f67d97
joaopmendes <41806813+joaopmendes@users.noreply.github.com>
2020-01-17 00:31:26 +0000

    Added cors middleware to backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,24 +1,33 @@
-const express = require("express")
-var bodyParser = require('body-parser')
-const routes = require(`./routes`)
-const mongoose = require("mongoose")
-require("dotenv").config()
+const express = require("express");
+var bodyParser = require("body-parser");
+const routes = require(`./routes`);
+const mongoose = require("mongoose");
+require("dotenv").config();
+const cors = require("cors");
 
-const app = express()
+const app = express();
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 try {
   mongoose.connect(encodeURI(process.env.MONGO_STRING), {
     useNewUrlParser: true,
     useUnifiedTopology: true
-  })
+  });
 } catch (e) {
-  console.log(e)
+  console.log(e);
 }
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
 
-app.use(routes)
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.listen(port)
+app.use(routes);
+
+app.listen(port);
